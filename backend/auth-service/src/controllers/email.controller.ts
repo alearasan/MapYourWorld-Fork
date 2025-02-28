7b5e3097581692e7bea28a6c936fc1a98c3b9329
antporara <antporara@alum.us.es>
2025-02-28 19:38:47 +0100

    style: Eliminar comentario innecesario

--- backend/auth-service/src/controllers/email.controller.ts
+++ backend/auth-service/src/controllers/email.controller.ts
@@ -8,45 +8,35 @@ import { text } from 'stream/consumers';
  */
 export const sendMail = async (req: Request, res: Response): Promise<void> => {
     try {
-        /**to: string;
-  subject: string;
-  html: string;
-  text?: string;
-  from?: string;
-  attachments?: Array<{
-    filename: string;
-    content: Buffer | string;
-    contentType?: string;
-   */
-    const { from, to, subject, html, text, attachments } = req.body;
-      if (!from || !to || !subject || !html) {
-        res.status(400).json({
-          success: false,
-          message: 'Los campos from, to, subject y html son requeridos'
-        });
-        return;
-      }
-      const mailOptions = { to, subject, html, text, from, attachments };
-      const result = await sendEmail(mailOptions);
+        const { from, to, subject, html, text, attachments } = req.body;
+        if (!from || !to || !subject || !html) {
+            res.status(400).json({
+            success: false,
+            message: 'Los campos from, to, subject y html son requeridos'
+            });
+            return;
+        }
+        const mailOptions = { to, subject, html, text, from, attachments };
+        const result = await sendEmail(mailOptions);
   
-      if (result.success) {
-        res.status(200).json({
-          success: true,
-          message: 'Correo enviado correctamente'
-        });
-      } else {
-        res.status(500).json({
-          success: false,
-          message: 'Error al enviar correo',
-          error: result.error?.message
-        });
-      }
+        if (result.success) {
+            res.status(200).json({
+            success: true,
+            message: 'Correo enviado correctamente'
+            });
+        } else {
+            res.status(500).json({
+            success: false,
+            message: 'Error al enviar correo',
+            error: result.error?.message
+            });
+        }
     } catch (error) {
-      res.status(500).json({
-        success: false,
-        message: 'Error al enviar correo',
-        error: error instanceof Error ? error.message : 'Error desconocido'
-      });
+        res.status(500).json({
+            success: false,
+            message: 'Error al enviar correo',
+            error: error instanceof Error ? error.message : 'Error desconocido'
+        });
     }
   };
 
