40698ac697df0b1ed195ef47babe9d3a7e37e98f
Manuel Vélez López <manvellop2@alum.us.es>
2025-03-04 18:31:44 +0100

    fix: ajustes en rutas de map

--- backend/map-service/src/routes/district.routes.ts
+++ backend/map-service/src/routes/district.routes.ts
@@ -1,11 +1,21 @@
 import { Router } from 'express';
-import { createDistrict, getDistrictById, getAllDistricts } from '@backend/map-service/src/controllers/district.controller';
+import { createDistrict, getDistrictById, getAllDistricts, unlockDistrict, getUserUnlockedDistricts, findDistrictContainingLocation } from '@backend/map-service/src/controllers/district.controller';
 
 const router: Router = Router();
 
-router.post('/', createDistrict);
-router.get('/', getAllDistricts);
-router.get('/:id', getDistrictById);
-// router.delete('/districts/:id', deleteDistrict);
+router.post('/district/create', createDistrict);
+
+router.get('/district/:districtId', getDistrictById);
+
+router.get('/districts', getAllDistricts);
+
+router.get('/districts/update/:districtId', getDistrictById);
+
+router.get('/districts/unlock/:districtId/:userId', unlockDistrict);
+
+router.get('/districts/unlock/:userId', getUserUnlockedDistricts);
+
+router.get('/districts/location/:latitude/:longitude', findDistrictContainingLocation);
+
 
 export default router;
