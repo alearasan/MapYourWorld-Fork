601761ebc04e1db959a492ee7122bce2312912d4
PedroPabloSantos <pedsandom@alum.us.es>
2025-03-09 17:13:37 +0100

    feat: Entidad mapa, inicializacion de la entida Map con sus atributos start #93

---
+++ backend/map-service/src/models/map.model.ts
@@ -0,0 +1,18 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Geometry } from 'geojson';
+
+@Entity('maps')
+export class Map {
+    @PrimaryGeneratedColumn('uuid')
+    id!: string;
+
+    @Column({ type: 'varchar', length: 255, nullable: false })
+    name!: string;
+
+    @Column({ type: 'text', nullable: true })
+    description!: string;
+
+    @Column({ type: 'date', nullable: false })
+    createdAt!: Date;
+
+}
