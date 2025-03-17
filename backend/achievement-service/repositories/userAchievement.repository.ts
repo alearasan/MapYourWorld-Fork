cd6267001e09cb8b5c37fb493cb30716959c59bb
alfalolan <alfalolan@alum.us.es>
2025-03-17 22:04:04 +0100

    fix: Hacer público el atributo userAchivement Repo
    Se ha realizado este cambio para poder utilizar los métodos heredados de Repository

--- backend/achievement-service/repositories/userAchievement.repository.ts
+++ backend/achievement-service/repositories/userAchievement.repository.ts
@@ -2,8 +2,8 @@ import { Repository } from 'typeorm';
 import { AppDataSource } from '../../database/appDataSource'; // Importa la instancia de conexión
 import { UserAchievement } from '../models/userAchievement.model';
 
-export default class userAchievementRepository {
-    private userAchievementRepo: Repository<UserAchievement>;
+export default class UserAchievementRepository {
+    public userAchievementRepo: Repository<UserAchievement>;
 
     constructor() {
         this.userAchievementRepo = AppDataSource.getRepository(UserAchievement);
