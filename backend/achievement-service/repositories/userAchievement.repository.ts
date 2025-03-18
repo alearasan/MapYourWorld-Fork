f84d59c6a2c3627929a4fd2880e6743cbb7b6bdc
Manuel Vélez López <manvellop2@alum.us.es>
2025-03-18 11:20:13 +0100

    fix: arreglos en userAchievements, Se ha realizado los arreglos pertinenetes para hacer los posteriores tests

--- backend/achievement-service/repositories/userAchievement.repository.ts
+++ backend/achievement-service/repositories/userAchievement.repository.ts
@@ -1,12 +1,51 @@
 import { Repository } from 'typeorm';
 import { AppDataSource } from '../../database/appDataSource'; // Importa la instancia de conexión
 import { UserAchievement } from '../models/userAchievement.model';
+import { Achievement } from '../models/achievement.model';
+import { User } from '../../auth-service/src/models/user.model';
 
-export default class UserAchievementRepository {
+export class UserAchievementRepository {
     public userAchievementRepo: Repository<UserAchievement>;
+    public achievementRepo: Repository<Achievement>
 
     constructor() {
         this.userAchievementRepo = AppDataSource.getRepository(UserAchievement);
+        this.achievementRepo = AppDataSource.getRepository(Achievement)
+        
     }
+
+    async createUserAchievement(userAchievementData: Omit<UserAchievement, 'id'>, achievementId:string): Promise<UserAchievement>{
+        const newUserAchievement = this.userAchievementRepo.create(userAchievementData)
+        
+        
+        const logro = await this.achievementRepo.findOne({where: {id:achievementId}})
+
+        if(!logro){
+            throw new Error(`El logro con id ${achievementId} no existe ` )
+        }
+
+        newUserAchievement.achievement = logro
+
+        return this.userAchievementRepo.save(newUserAchievement)
+
+
+    }
+
+    async getAchievementsByUserId(userId: string): Promise<UserAchievement[]>{
+            const logrosUsuario = await this.userAchievementRepo.find({ where: { user: { id: userId }}})
+        return logrosUsuario
+    }
+
+
+    async getUsersByAchievementId(achievementId:string): Promise<User[] | null>{
+
+        const usuariosConLogros = await this.userAchievementRepo.find({ where: { achievement: { id: achievementId } }})
+
+        return usuariosConLogros.map(userAchievement => userAchievement.user)
+    }
+
+        
+
+        
 
 }
--- backend/achievement-service/services/userAchievement.service.ts
+++ backend/achievement-service/services/userAchievement.service.ts
@@ -5,9 +5,10 @@
 
 //import { publishEvent } from '@shared/libs/rabbitmq';
 import {UserAchievement} from '../models/userAchievement.model';
-import UserAchievementRepository from '../repositories/userAchievement.repository';
+import {UserAchievementRepository} from '../repositories/userAchievement.repository';
+import { User } from '../../auth-service/src/models/user.model';
 
-const repo = new UserAchievementRepository().userAchievementRepo;
+const repo = new UserAchievementRepository();
 
 /**
  * Crea un nuevo logro
@@ -17,12 +18,13 @@ const repo = new UserAchievementRepository().userAchievementRepo;
 //crear logro
 export const createUserAchievement = async (
   UserAchievementData: Omit<UserAchievement, 'id'>,
+  achievementId: string
 ): Promise<UserAchievement> => {
   try {
     if (!UserAchievementData.user || !UserAchievementData.achievement || !UserAchievementData.dateEarned) {
       throw new Error("No pueden faltar datos de user-achievement.")
     }
-    const newUserAchievement = repo.create(UserAchievementData);
+    const newUserAchievement = repo.createUserAchievement(UserAchievementData, achievementId);
 
     console.log("uusario-logro creado correctamente:", newUserAchievement);
     return newUserAchievement;
@@ -32,33 +34,24 @@ export const createUserAchievement = async (
   }
 };
 
-//obtener todos los logros
-export const getAchievements = async (): Promise<UserAchievement[]> => {
-  try {
-    const achievements = await repo.find();
-    return achievements;
-  } catch (error) {
-    throw new Error("Error al obtener los logros");
-  }
-};
 
 //obtener todos los logros de un usuario
 
 export const getAchievementsByUser = async (userId: string): Promise<UserAchievement[]> => {
   try {
-    const userAchievements = await repo.find({ where: { user: { id: userId } }
-    });
+    const userAchievements = await repo.getAchievementsByUserId(userId)
     return userAchievements;
-  } catch (error) {
+
+  }
+    catch (error) {
     throw new Error("Error al obtener los logros del usuario");
   }
-};
+}
 
 //obtener todos usuarios que consiguieron un logro
-export const getUsersByAchievement = async (achievementId: string): Promise<UserAchievement[]> => {
+export const getUsersByAchievement = async (achievementId: string): Promise<User[] | null> => {
   try {
-    const userAchievements = await repo.find({ where: { achievement: { id: achievementId } }
-    });
+    const userAchievements = await repo.getUsersByAchievementId(achievementId) 
     return userAchievements;
   } catch (error) {
     throw new Error("Error al obtener los logros del usuario");
